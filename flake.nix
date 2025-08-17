a09502ffb0d1f7d0f92213c7da66a0bd6326a6b7
Francesco Ceccon <francesco@ceccon.me>
2025-08-17 20:39:10 +0200

    ci: prepare for release

--- flake.nix
+++ flake.nix
@@ -26,36 +26,140 @@
           inherit system overlays;
         };
 
-        crates = {};
-
-        buildArtifacts = pkgs.callPackage ./nix/build.nix {
-          inherit crane crates;
-          workspaceDir = ./.;
+        rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [ "rust-src" "rust-analyzer" ];
         };
 
-        ci = pkgs.callPackage ./nix/ci.nix { };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = pkgs.lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = path: type:
+            (builtins.match ".*proto$" path != null)
+            || (craneLib.filterCargoSources path type);
+        };
+
+        commonArgs = {
+          inherit src;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            protobuf
+            openssl.dev
+          ] ++ pkgs.lib.optional stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+            Security
+            SystemConfiguration
+          ]);
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+          pname = "wings";
+          version = "0.0.0";
+        });
+
+        binary = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          pname = "wings";
+          version = "0.0.0";
+          doCheck = false;
+        });
+
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "ghcr.io/apibara/wings";
+          tag = "latest";
+          created = "now";
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [ binary ];
+            pathsToLink = [ "/bin" ];
+          };
+          config = {
+            Cmd = [ "/bin/wings" ];
+            ExposedPorts = {};
+          };
+        };
+
+        dockerArchive = pkgs.stdenv.mkDerivation {
+          name = "wings-image";
+          buildInputs = [
+            pkgs.skopeo
+          ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out
+            echo '{"default": [{"type": "insecureAcceptAnything"}]}' > /tmp/policy.json
+            skopeo copy --policy=/tmp/policy.json --tmpdir=/tmp docker-archive:${dockerImage} docker-archive:$out/wings.tar.gz
+          '';
+        };
+
+        publishDockerImage = pkgs.writeScriptBin "publish-docker-image" ''
+          set -euo pipefail
+
+          echo "Ref: $GITHUB_REF"
+
+          if [[ "''${GITHUB_REF:-}" != "refs/tags/"* ]]; then
+            echo "Not a tag"
+            exit 0
+          fi
+
+          if ! [ -f "''${IMAGE_ARCHIVE_x86_64}" ]; then
+            echo "IMAGE_ARCHIVE_x86_64 image does not exist"
+            exit 1
+          fi
+
+          if ! [ -f "''${IMAGE_ARCHIVE_aarch64}" ]; then
+            echo "IMAGE_ARCHIVE_aarch64 image does not exist"
+            exit 1
+          fi
+
+          skopeo="${pkgs.skopeo}/bin/skopeo"
+          echo '{"default": [{"type": "insecureAcceptAnything"}]}' > /tmp/policy.json
+
+          echo "::group::Logging in to ghcr.io"
+          echo "::add-mask::''${GHCR_USERNAME}"
+          echo "::add-mask::''${GHCR_PASSWORD}"
+
+          skopeo login -u="''${GHCR_USERNAME}" -p="''${GHCR_PASSWORD}" ghcr.io
+          echo "::endgroup::"
+
+          tag=''${GITHUB_REF#refs/tags/}
+          base="ghcr.io/apibara/wings"
+
+          echo "::group::Pushing docker image to $''${tag}"
+          skopeo copy "docker-archive:''${IMAGE_ARCHIVE_x86_64}" "docker://''${base}:''${tag}-x86_64"
+          skopeo copy "docker-archive:''${IMAGE_ARCHIVE_aarch64}" "docker://''${base}:''${tag}-aarch64"
+          echo "::endgroup::"
+
+          images=("''${base}:''${tag}-x86_64" "''${base}:''${tag}-aarch64")
+          echo "::group::Create manifest ''${base}:''${tag}"
+          manifest="''${base}:''${tag}"
+          buildah manifest create "''${manifest}" "''${images[@]}"
+          echo "::endgroup::"
+        '';
       in
       {
-        # format with `nix fmt`
-        formatter = pkgs.nixpkgs-fmt;
-
-        # checks. run with `nix flake check`.
-        checks = buildArtifacts.checks;
+        packages = {
+          default = binary;
+          image = dockerArchive;
+        };
 
         # development shells. start with `nix develop`.
-        devShells = (buildArtifacts.shell // ci.shell // { });
-
-        # all packages.
-        # show them with `nix flake show`.
-        # build with `nix build .#<name>`.
-        packages = (buildArtifacts.packages // { });
+        devShells = {
+          default = pkgs.mkShell {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.openssl
+            ];
+            inputsFrom = [ binary ];
+            nativeBuildInputs = with pkgs; [
+              opentelemetry-collector
+            ];
+          };
+          ci = pkgs.mkShell {
+            buildInputs = [ publishDockerImage ];
+          };
+        };
       }
     );
-
-  nixConfig = {
-    extra-substituters = [ "https://apibara-public.cachix.org" ];
-    extra-trusted-public-keys = [
-      "apibara-public.cachix.org-1:FLOMNlARo9CcxtcLDblZlt2xhsu/pa/EddEH1cM3Vog="
-    ];
-  };
 }
