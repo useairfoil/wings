304eb787b30a07957633c0dc2d67123e70edc98a
Francesco Ceccon <francesco@ceccon.me>
2026-02-11 17:50:57 +0800

    object-store: percent encode partition values in path

--- wings_object_store/src/paths.rs
+++ wings_object_store/src/paths.rs
@@ -1,3 +1,4 @@
+use percent_encoding::utf8_percent_encode;
 use wings_resources::{NamespaceName, PartitionValue, TopicName};
 
 pub fn format_folio_path(namespace: &NamespaceName, folio_id: &str) -> String {
@@ -14,9 +15,13 @@ pub fn format_partitioned_parquet_data_path(
     partition: &Option<PartitionValue>,
     file_id: &str,
 ) -> String {
+    use percent_encoding::NON_ALPHANUMERIC;
+
     let partition_value = match partition {
         None => "null".to_string(),
-        Some(partition) => partition.to_string(),
+        Some(partition) => {
+            utf8_percent_encode(&partition.to_string(), NON_ALPHANUMERIC).to_string()
+        }
     };
 
     format!(
